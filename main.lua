d694e2072e4b38a53e909fc72f4e24b7b1e56db4
George Cruz <gcflex20gmail.com>
2025-04-26 23:17:40 -0400

    some message

--- main.lua
+++ main.lua
@@ -1,11 +1,15 @@
+--git add .
+--git commit -m "some message"
+--git push
+
 function love.load()
 
 end
 
 function love.update(dt)
-
+    
 end
 
 function love.draw()
-    
+    love.graphics.print('Hello World!', 400, 300)
 end
